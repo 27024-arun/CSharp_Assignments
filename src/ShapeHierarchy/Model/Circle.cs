7fbdaa6107ccdebbbe003cb03898e61a1d4b4760
ARUN <arun.sekar@solitontech.com>
2026-07-20 19:23:23 +0530

    feat: ShapeHierarchy is done

---
+++ src/ShapeHierarchy/Model/Circle.cs
@@ -0,0 +1,34 @@
+﻿namespace ShapeHierarchy.Model
+{
+    /// <summary>
+    /// Circle class defines the shape circle and inherits ShapeModel class
+    /// </summary>
+    internal class Circle : ShapeModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Circle"/> class.
+        /// Circle method is the constructor of the Circle class
+        /// </summary>
+        /// <param name="color">Color is the color of the cirlce</param>
+        /// <param name="radius">Radius is the radius of the cirlce</param>
+        public Circle(string color, double radius)
+        {
+            Color = color;
+            Radius = radius;
+        }
+
+        /// <summary>
+        /// Gets or sets the radius of the circle
+        /// </summary>
+        /// <value>
+        /// The radius of the circle
+        /// </value>
+        public double Radius { get; set; }
+
+        /// <summary>
+        /// CalculateArea method is the function that defines the area calculation of the circle
+        /// </summary>
+        /// <returns>Returns the area of the cirlce</returns>
+        public override double CalculateArea() => Math.PI * Radius * Radius;
+    }
+}
---
+++ src/ShapeHierarchy/Model/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿namespace ShapeHierarchy.Model
+{
+    /// <summary>
+    /// Rectangle is the class that has the properties of rectangle shape and inherits ShapeModel class
+    /// </summary>
+    internal class Rectangle : ShapeModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Rectangle"/> class.
+        /// Retangle method is the constructor for the Rectangle class
+        /// </summary>
+        /// <param name="color">Color is the color of the rectangle</param>
+        /// <param name="width">Width is the width of the rectangle</param>
+        /// <param name="height">Height is the height of the rectangle</param>
+        public Rectangle(string color, double width, double height)
+        {
+            Color = color;
+            Width = width;
+            Height = height;
+        }
+
+        /// <summary>
+        /// Gets or Sets the width of the shape
+        /// </summary>
+        /// <value>
+        /// Width of the shape
+        /// </value>
+        public double Width { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the height of the shape
+        /// </summary>
+        /// <value>
+        /// Height of the shape
+        /// </value>
+        public double Height { get; set; }
+
+        /// <summary>
+        /// CalculateArea functions is used to calculate the area of the rectangle
+        /// </summary>
+        /// <returns>Returns the area of the rectangle</returns>
+        public override double CalculateArea() => Width * Height;
+    }
+}
