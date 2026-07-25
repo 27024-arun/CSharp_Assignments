09cb62bb1c8e02508d36051987e68d5d29228713
ARUN <arun.sekar@solitontech.com>
2026-07-25 11:48:02 +0530

    feat: Structure is created

---
+++ src/Inventory Management/Model/InventoryModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Inventory_Management.Model
+{
+    internal class InventoryModel
+    {
+    }
+}
---
+++ src/Inventory Management/Services/InventoryServices.cs
@@ -0,0 +1,6 @@
+﻿namespace Inventory_Management.Services
+{
+    internal class InventoryServices
+    {
+    }
+}
