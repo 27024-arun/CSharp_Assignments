9a9180cc937bb309551e0d0c32a6c369c587a4a3
ARUN <arun.sekar@solitontech.com>
2026-08-26 20:21:31 +0530

    feat: Task 1 of Assignment 9 implemented

---
+++ src/LINQPractices/Helper/ProductAdder.cs
@@ -0,0 +1,28 @@
+﻿using LINQPractices.Model;
+
+namespace LINQPractices
+{
+    internal class ProductAdder
+    {
+        public List<Product> Add()
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product("100", "Headset", 1500, ProductCategory.Electronics));
+            products.Add(new Product("101", "Shampoo", 100, ProductCategory.Cosmetics));
+            products.Add(new Product("102", "Knife", 1500, ProductCategory.Utensils));
+            products.Add(new Product("103", "Bandage", 20, ProductCategory.Medicine));
+            products.Add(new Product("104", "Frypan", 3700, ProductCategory.Utensils));
+            products.Add(new Product("105", "Spatula", 450, ProductCategory.Utensils));
+            products.Add(new Product("106", "Cookies", 600, ProductCategory.Snacks));
+            products.Add(new Product("107", "Waterbottle", 850, ProductCategory.Utensils));
+            products.Add(new Product("108", "Monitor", 14500, ProductCategory.Electronics));
+            products.Add(new Product("109", "Paracetamol Strip", 120, ProductCategory.Medicine));
+            products.Add(new Product("110", "Chips", 1200, ProductCategory.Snacks));
+            products.Add(new Product("111", "Perfume", 2500, ProductCategory.Cosmetics));
+            products.Add(new Product("112", "Chocolate", 1500, ProductCategory.Snacks));
+            products.Add(new Product("113", "Toaster", 5000, ProductCategory.Electronics));
+            products.Add(new Product("114", "Mouse", 500, ProductCategory.Electronics));
+            products.Add(new Product("115", "Soap", 200, ProductCategory.Cosmetics));
+        }
+    }
+}
---
+++ src/LINQPractices/Model/Supplier.cs
@@ -0,0 +1,18 @@
+﻿namespace LINQPractices.Model
+{
+    internal class Supplier
+    {
+        public Supplier(int supplierID, string supplierName, int productId)
+        {
+            this.SupplierID = supplierID;
+            this.SupplierName = supplierName;
+            this.ProductId = productId;
+        }
+
+        public int SupplierID { get; set; }
+
+        public string SupplierName { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
