9daba170e02bbfdf83b4e252945a3aa8422d4c8e
ARUN <arun.sekar@solitontech.com>
2026-08-26 20:21:31 +0530

    feat: Task 1 of Assignment 9 implemented

---
+++ src/LINQPractices/ComplexLinqTask.cs
@@ -0,0 +1,27 @@
+﻿using LINQPractices.Model;
+
+namespace LINQPractices
+{
+    internal class ComplexLinqTask
+    {
+        private readonly List<Product> _products;
+
+        public ComplexLinqTask(List<Product> products)
+        {
+            this._products = products;
+        }
+
+        internal void GroupData()
+        {
+            var groupedData = this._products.GroupBy(product => product.Category);
+            foreach (var groups in groupedData)
+            {
+                Console.WriteLine($"Group Category: {groups.Key}");
+                foreach (var value in groups)
+                {
+                    Console.WriteLine($"\nProduct Id: {value.ProductId}\nProduct Name: {value.ProductName}\nProduct Price: {value.Price}\n");
+                }
+            }
+        }
+    }
+}
