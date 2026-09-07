cd7bc714bd7cea3f74946cc7358e9543c6d48518
ARUN <arun.sekar@solitontech.com>
2026-09-07 14:19:50 +0530

    feat: Memory optimization is implemented

---
+++ src/MemoryOptimization/MemoryModifier.cs
@@ -0,0 +1,49 @@
+﻿namespace MemoryOptimization
+{
+    /// <summary>
+    /// Makes memory not to go out of bound.
+    /// </summary>
+    internal class MemoryModifier : IDisposable
+    {
+        private readonly int _maxData;
+        private List<int[]>? _memAlloc;
+        private bool _isDisposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemoryModifier"/> class.
+        /// </summary>
+        /// <param name="maxData">Maximum data to be stored in the list.</param>
+        public MemoryModifier(int maxData = 10000)
+        {
+            this._maxData = maxData;
+            this._memAlloc = new List<int[]>();
+            this._isDisposed = false;
+        }
+
+        /// <summary>
+        /// Allocates memory and adds it to a list.
+        /// </summary>
+        public void Allocate()
+        {
+            while (!this._isDisposed)
+            {
+                this._memAlloc.Add(new int[1000]);
+                if (this._memAlloc.Count > this._maxData)
+                {
+                    this.Dispose();
+                }
+
+                Console.WriteLine($"Current Memory Usage: {GC.GetAllocatedBytesForCurrentThread() / 1024 / 1024}Mb");
+                Thread.Sleep(10);
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this._memAlloc = null;
+            this._isDisposed = true;
+            GC.Collect();
+        }
+    }
+}
---
+++ src/MemoryOptimization/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MemoryOptimization
+{
+    /// <summary>
+    /// Application entry point.
+    /// </summary>
+    internal class Program
+    {
+        private static void Main()
+        {
+            // MemoryEater memoryEater = new MemoryEater();
+            // memoryEater.Allocate();
+            using (MemoryModifier memoryModifier = new MemoryModifier())
+            {
+                memoryModifier.Allocate();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
